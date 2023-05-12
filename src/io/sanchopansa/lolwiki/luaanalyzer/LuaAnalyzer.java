14890a5c810377d13e4fc59ae76f0079c3a2e8ef
SanchoPansa <zelgadis4@gmail.com>
2023-05-12 18:37:10 +0300

    Added regex processor for "require" command

--- src/io/sanchopansa/lolwiki/luaanalyzer/LuaAnalyzer.java
+++ src/io/sanchopansa/lolwiki/luaanalyzer/LuaAnalyzer.java
@@ -1,5 +1,11 @@
 package io.sanchopansa.lolwiki.luaanalyzer;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LuaAnalyzer {
 
     public LuaAnalyzer() {
@@ -7,6 +13,12 @@ public class LuaAnalyzer {
     }
 
     public void analyzeLuaDependencies(String code) {
-        
+        System.out.println(code);
+        String requirePattern = "require *?\\((\\\\\"|')(.*?)(\\\\\"|')\\)";
+        Pattern pattern = Pattern.compile(requirePattern);
+        Matcher matcher = pattern.matcher(code);
+
+        List<String> requiresList = matcher.results().map(a -> a.group(2)).toList();
+        requiresList.forEach(System.out::println);
     }
 }
--- src/io/sanchopansa/lolwiki/luaanalyzer/MainClass.java
+++ src/io/sanchopansa/lolwiki/luaanalyzer/MainClass.java
@@ -2,6 +2,8 @@ package io.sanchopansa.lolwiki.luaanalyzer;
 
 public class MainClass {
     public static void main(String[] args) {
-        System.out.println(new LuaFetcher("Модуль:SkinData").getLuaCode());
+        String code = new LuaFetcher("Модуль:SkinData").getLuaCode();
+        LuaAnalyzer analyzer = new LuaAnalyzer();
+        analyzer.analyzeLuaDependencies(code);
     }
 }
